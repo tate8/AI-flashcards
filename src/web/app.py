02a3a152ca93a9b6c9c7e873ddd2f2c660661cb3
Tate Larkin <tatelarkin@Tates-MacBook-Pro.local>
2022-02-09 09:51:43 -0700

    draw functionality and random words

--- src/web/app.py
+++ src/web/app.py
@@ -1,11 +1,22 @@
-from flask import Flask, render_template, redirect, url_for
-
+from flask import Flask, render_template, redirect, url_for, request
+import random
 
 app = Flask(__name__)
+
+
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template('index.html')
 
+@app.route('/cards')
+def cards():
+    cat_file = open("categories.txt", "r")
+    content_list = cat_file.readlines()
+    word = random.choice(content_list)
+
+    message = f'Can you draw {word}?'
+
+    return render_template('cards.html', word=word, message=message)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=3000)
