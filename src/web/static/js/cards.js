6ad0d29029ab639544bbdb855f1885914e5cfb2c
Tate Larkin <tatelarkin@Tates-MacBook-Pro.local>
2022-02-11 19:27:43 -0700

    pass image data to server and format for model to take

--- src/web/static/js/cards.js
+++ src/web/static/js/cards.js
@@ -1,4 +1,3 @@
-/// --------------------------------ctx.getImageData(sx, sy, sw, sh);------------------------------------
 
 let eraser = document.querySelector('.eraser')
 let pencil = document.querySelector('.pencil')
@@ -17,28 +16,24 @@ let setPencilActive = function() {
     pencilActive = true;
 }
 
-
 // change drawing tool
 eraser.addEventListener('click', setEraserActive);
 pencil.addEventListener('click', setPencilActive);
 
 
-
-// DRAW ON FLASHCARD CAPABILITY
-//
-//
+// get handle to canvas element
 const paintCanvas = document.querySelector( '.paint-canvas' );
 const context = paintCanvas.getContext( '2d' );
 
 // get flashcard dimensions and set canvas's dimensions to them
 width = document.querySelector('.flashcard').clientWidth;
 height = document.querySelector('.flashcard').clientHeight;
-
 context.canvas.width = width;
 context.canvas.height = height;
 
 
-let clearCanvas = function() {
+let clearCanvas = function()
+{
     context.clearRect(0, 0, context.canvas.width, context.canvas.height);
 }
 clear.addEventListener('click', clearCanvas);
@@ -48,32 +43,43 @@ clear.addEventListener('click', clearCanvas);
 let x = 0, y = 0;
 let isMouseDown = false;
 
+// on mouse out/up
 const stopDrawing = () => { isMouseDown = false; }
-const startDrawing = event => {
+
+// on mouse down
+const startDrawing = event =>
+{
     isMouseDown = true;   
-[x, y] = [event.offsetX, event.offsetY];  
+    [x, y] = [event.offsetX, event.offsetY];  
 }
-const drawLine = event => {
-    if ( isMouseDown && pencilActive ) {
+
+// on mouse move
+const drawLine = event =>
+{
+    // checks if pencil mode is on
+    if ( isMouseDown && pencilActive )
+    {
         const newX = event.offsetX;
         const newY = event.offsetY;
         context.beginPath();
         context.strokeStyle = 'black'
         context.lineCap = 'round';
-        context.lineWidth = 10;
+        context.lineWidth = 20;
         context.moveTo( x, y );
         context.lineTo( newX, newY );
         context.stroke();
         x = newX;
         y = newY;
     }
-    else if (isMouseDown && !pencilActive) {
+    // checks if erase mode is on
+    else if (isMouseDown && !pencilActive)
+    {
         const newX = event.offsetX;
         const newY = event.offsetY;
         context.beginPath();
-        context.strokeStyle = 'white'
+        context.strokeStyle = 'white'   // drawing white to 'erase'
         context.lineCap = 'round';
-        context.lineWidth = 30;
+        context.lineWidth = 30;         // slightly bigger
         context.moveTo( x, y );
         context.lineTo( newX, newY );
         context.stroke();
@@ -87,7 +93,35 @@ paintCanvas.addEventListener( 'mousemove', drawLine );
 paintCanvas.addEventListener( 'mouseup', stopDrawing );
 paintCanvas.addEventListener( 'mouseout', stopDrawing );
 
+function checkButtonClicked()
+{
+    let canvasHeight = context.canvas.clientHeight;
+    let canvasWidth = context.canvas.clientWidth;
+    let imageData = context.getImageData(0, 0, canvasWidth, canvasHeight);
+ 
+    let greyImageData = []
 
-// CLEAR FUNCTIONALITY
+    // each pixel in image data is              [R, G, B, A]
+    // all black pixels are represented as      [0, 0, 0, 255]
 
-let clearButton = document.querySelector('')
+    // this just parses out the all black pixels and stores them into greyImageData
+    for (let i = 3; i < imageData.data.length; i += 4) {
+        let greyPixel = imageData.data[i]
+
+        greyImageData.push(greyPixel)
+    }
+
+    // send an ajax POST request to my flask server with all the pixel data
+    $.ajax({
+        url: '/post-pixel-data',
+        type: 'POST',
+        ContentType: 'application/json',
+        data: {data: greyImageData}
+      }).fail(function(jqXHR, textStatus, errorThrown)
+      { // if ajax POST request fails
+        alert('Something went wrong. error: ' + errorThrown);
+      });
+}
+
+let checkButton = document.querySelector('.check-word');
+checkButton.addEventListener( 'click', checkButtonClicked);
